ed4e17ef18b2a2283866851c1cab2c9f8673a678
OCN.Yang <yangocn@163.com>
2018-03-23 11:18:19 +0800

    create ViewAnimation;

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Android-Animation-Set
-android 动画详尽教程
+# Android 动画详尽教程
+
+## Ⅰ. View Animation / 视图动画
+
+## Ⅱ. Drawable Animation / 帧动画 / Frame 动画
+
+## Ⅲ. Property Animation / 属性动画  
+
+## Ⅳ. 触摸反馈动画
+
+## Ⅴ. 揭露动画
+
+## Ⅵ. 转场动画 & 共享元素
+
+## Ⅶ. 视图状态动画
+
+## Ⅷ. 矢量图动画
--- app/src/main/java/com/ocnyang/androidanimationset/MainActivity.java
+++ app/src/main/java/com/ocnyang/androidanimationset/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ocnyang.androidanimationset;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, com.ocnyang.viewanimation.MainActivity.class));
+            }
+        });
     }
 }
